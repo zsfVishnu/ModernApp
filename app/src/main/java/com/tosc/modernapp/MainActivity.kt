b94dfdee75ac7710a9457964fa24288f9be94065
zsfVishnu <vishnu.tp32@gmail.com>
2022-11-06 11:31:48 +0530

    feat: new app init commit

---
+++ app/src/main/java/com/tosc/modernapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tosc.modernapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
